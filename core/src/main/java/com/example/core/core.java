0f798ee6eea32c2f8380fb50716fb5e932f4dbfc
vahe.muradyan.03 <vahe.muradyan.03@gamil.com>
2024-08-06 03:31:18 +0400

    никак не могу

---
+++ core/src/main/java/com/example/core/core.java
@@ -0,0 +1,11 @@
+package com.example.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class core {
+    public static void main(String[] args) {
+        SpringApplication.run(core.class,args);
+    }
+}
